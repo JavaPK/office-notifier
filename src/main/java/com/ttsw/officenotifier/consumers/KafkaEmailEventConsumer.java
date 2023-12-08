57c198ce7f534b6e2724183e81f5110c261a98e0
Michal Witczak <michal.witczak666@gmail.com>
2023-12-08 11:11:09 +0100

    michal witczak - sending emails

--- src/main/java/com/ttsw/officenotifier/consumers/KafkaEmailEventConsumer.java
+++ src/main/java/com/ttsw/officenotifier/consumers/KafkaEmailEventConsumer.java
@@ -1,17 +1,34 @@
 package com.ttsw.officenotifier.consumers;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ttsw.officenotifier.events.MailEvent;
+import com.ttsw.officenotifier.mail.MailService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 @Service
+@RequiredArgsConstructor
 public class KafkaEmailEventConsumer {
 
-    @KafkaListener(topics = "${office-core.kafka.topics.email.name}", groupId = "office-notifier-group")
+
+    private final MailService mailSender;
+
+    private final ObjectMapper objectMapper;
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @SneakyThrows
+    @KafkaListener(topics = "${office-notifier.kafka.topics.email.name}", groupId = "office-notifier-group")
     public void consumeEvent(@Payload String message) {
-        System.out.println(String.format("Content: %s", message));
+        var mailEvent = objectMapper.readValue(message, MailEvent.class);
+        mailSender.sendEmail(mailEvent);
+        redisTemplate.opsForValue().append(Instant.now().toString(), message);
     }
 }
